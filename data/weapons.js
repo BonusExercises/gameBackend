7fd45ee50852a3620fe89f6e963fa94f11e59d12
MartinK <82144965+MartinKruess@users.noreply.github.com>
2023-09-21 23:12:42 +0200

    prepare server side mainpage (#1)
    Co-authored-by: MartinK <82144965+RaikunGZ@users.noreply.github.com>

---
+++ data/weapons.js
@@ -0,0 +1,82 @@
+export const Weapons = [
+    {
+        weaponName: "Einfacher Dolch (Dolch)",
+        attack: 13,
+        effect: "",
+    },
+    {
+        weaponName: "Haifischzahn (Dolch)",
+        attack: 21,
+        effect: "",
+    },
+    {
+        weaponName: "Klinge des Diebes (Dolch)",
+        attack: 28,
+        effect: "Erhöht die Ausbeute von Rubinen aus Kämpfen um 1",
+    },
+    {
+        weaponName: "Schattenschächter (Dolch)",
+        attack: 37,
+        effect: "",
+    },
+    {
+        weaponName: "Holzschwert (Schwert)",
+        attack: 16,
+        effect: "",
+    },
+    {
+        weaponName: "Schildbrecher (Schwert)",
+        attack: 23,
+        effect: "",
+    },
+    {
+        weaponName: "Holzschwert (Schwert)",
+        attack: 31,
+        effect: "",
+    },
+    {
+        weaponName: "Holzschwert (Schwert)",
+        attack: 40,
+        effect: "",
+    },
+    {
+        weaponName: "Holzbogen (Bogen)",
+        attack: 19,
+        effect: "",
+    },
+    {
+        weaponName: "Eiserner Bogen (Bogen)",
+        attack: 28,
+        effect: "",
+    },
+    {
+        weaponName: "Geheiligter Weidenbogen (Bogen)",
+        attack: 36,
+        effect: "Erhöht die Wahrscheinlichkeit auf einen Kritischentreffer um 5%",
+    },
+    {
+        weaponName: "Komponentbogen des Jägers (Bogen)",
+        attack: 44,
+        effect: "",
+    },
+    {
+        weaponName: "Einfacher Stab (Stab)",
+        attack: 18,
+        effect: "",
+    },
+    {
+        weaponName: "Wiedenstab der Sünde (Stab)",
+        attack: 24,
+        effect: "",
+    },
+    {
+        weaponName: "Kristallstab (Stab)",
+        attack: 32,
+        effect: "Wandelt 2% des verursachten Schadens in Leben um",
+    },
+    {
+        weaponName: "Uralterstab des Hexers (Stab)",
+        attack: 39,
+        effect: "",
+    },
+]
